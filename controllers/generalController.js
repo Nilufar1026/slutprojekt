1fbdca7e1e531bbb1ceb2d88f9fe65b8ddfc53a2
Romina Rebolledo <rominarebolledo@gmail.com>
2021-05-07 10:49:00 +0200

    Added function updateProfile

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -16,7 +16,32 @@ module.exports = {
         const user = await User.findOne({ where: { email } })
         const name = user.name  
         res.json({ "email": email, "name": name})
-    },
+    },  
 
-     
+    async updateProfile( req, res, next){
+        const email = res.user.email
+        try{
+            const { newName, newPassword } = req.body
+            if(!email || !newName || !newPassword) {
+                throw new InvalidBody()
+            }
+        } catch(error) {next(error)}
+        
+    }
+
+
+    // async updatePassword(req, res, next) {
+    //     const email = res.user.email
+    //     try {
+    //         const { newPassword } = req.body
+    //         if (!email || !newPassword) {
+    //             throw new InvalidBody()
+    //         } else {
+    //             const newPassHash = bcrypt.hashSync(newPassword, 10)
+    //             const newPass = await User.findOne({ where: { email } })
+    //             newPass.password = newPassHash
+    //             await newPass.save()
+    //             res.send({ msn: "Password updated successfully!" })
+    //         }
+    //     } catch (error) { next(error) }
 }
