68b913214aa969bef48259ae7b49c504c5df20f8
Nilufar1026 <71069308+Nilufar1026@users.noreply.github.com>
2021-05-11 14:25:38 +0200

    added add image controller, edit worker id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const fileUpload = require('express-fileupload');
 
 const generalRoutes = require('./routes/general')
 const adminRoutes = require('./routes/admin')
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.use(fileUpload());
 
 app.use(generalRoutes)
 app.use(adminRoutes)
