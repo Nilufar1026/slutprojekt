f6f4200ada5a19006de1753317a98ad45f551d70
splarsxd <splarsiz34@yahoo.com>
2021-05-11 14:05:29 +0200

    commit

---
+++ controllers/clientController.js
@@ -0,0 +1,28 @@
+const User = require("../models/User")
+const Task = require("../models/Tasks")
+
+module.export = {
+    async getClient(req, res, next){
+        try{
+
+        } catch{
+
+        }
+    },
+
+    async newMessage(req, res, next){
+        try{
+
+        } catch{
+
+        }
+    },
+
+    async deleteMessage(req, res, next){
+        try{
+
+        } catch{
+            
+        }
+    }
+}
---
+++ routes/client.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const Routes = express.Router()
+const clientControl = require('../controllers/clientController')
+const Auth = require('../middlewares/auth')
+
+
+Routes.get('/task', Auth.user, clientControl.getTask)
+Routes.get('/tasks/:id', Auth.user, clientControl.getTaskById)
+Routes.get('/tasks/:id/messages', Auth.user, clientControl.getClient)
+Routes.post('/tasks/:id/messages', Auth.user, clientControl.newMessage)
+Routes.delete('/tasks/:id/messages', Auth.user, clientControl.deleteMessage)
