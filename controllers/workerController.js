9243832831ada79fcaa070151a5d5c1d618f175e
nilufar <nilux1026@gmail.com>
2021-05-12 14:59:36 +0200

    fixed some endpoints

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -1,8 +1,10 @@
-const {InvalidBody,taskNotFound}=require('../errors/index')
+const {InvalidBody,TaskNotFound,UserNotFound}=require('../errors/index')
 
 const Task=require('../models/Tasks')
+const User=require('../models/User')
 const path=require('path')
 const { v4: uuid } = require('uuid');
+const Tasks = require('../models/Tasks');
 
 module.exports={
     async create(req,res,next){
@@ -18,48 +20,81 @@ module.exports={
     },
 
     async addImage(req,res,next){
-
         try{
             const {id}=req.params
             const file=req.files.pictures
-            
-
-            console.log(file)
             if( !file ){
                 throw new InvalidBody(['file'])
             }
-           
 
             const findTask = await Task.findOne({where:{id}})
-            if(!findTask){ throw new taskNotFound(id) }
-            
-            // give unic name for the file.
+            if(!findTask){ throw new TaskNotFound(id) }
             const extension=path.extname(file.name)
             const newFileName=uuid()+extension
-
             const outputPath=path.join("upload_images",newFileName)
-         
 
-             file.mv(outputPath,  (err) => {
+            file.mv(outputPath,  (err) => {
                 if (err) return res.status(500).send(err)
                 Task.update(
                     {imageName:newFileName},
                     {where:{id}}
                 );
-                //await newImage.reload();
             res.json({message:'image has added!'}) 
             })
         }catch(error){next(error)}
     },
 
+    async getTaskByClientName(req, res, next) {
+        const {name} = req.query
+        try {
+            const user = await User.findOne({where: {name}})
+            if(!user){ throw new UserNotFound() }
+            const userId=user.id
+            const tasks=await Task.findAll({
+                    where: {
+                        clientId:userId
+                    }
+              });
 
-  
-      
-        
+            res.json({ tasks })
+        } catch (error) { next(error) }
+    },
 
-    async getTaskByClientName(req,res,next){
+
+
+    
+    async updateTaskById(req,res,next){
+        try{
+            const {id}=req.params
+            const {taskName,clientId} = req.body
+            const {done}=req.query
+            const field={}
+
+            if(taskName) field.taskName=taskName
+            if(clientId) field.clientId=clientId
+            if(done) field.done=done
+
+            const findTask = await Task.findOne({where:{id}})
+            if(!findTask){ throw new TaskNotFound(id) }
+            
+            
+            await Task.update(field,{where:{id}})
+            res.json({message:'Task has pdated'})
+        }catch(error){next(error)}
 
     },
+
+    async getTaskById(req,res,next){
+        try {
+            const { id } = req.params
+            const task = await Task.findOne({
+                where: {id}
+            })
+            if (!task) { throw new TaskNotFound() }
+            res.json({ task })
+        } catch (error) { next(error) }
+    },
+    
 
 
 }
--- routes/worker.js
+++ routes/worker.js
@@ -9,17 +9,17 @@ router.post('/tasks',Auth.worker,workerController.create) //Skapar ett nytt äre
 
 router.post('/tasks/:id/image',Auth.worker,workerController.addImage) //Laddar upp en bild på ärendet
 
-// router.get('/tasks',Auth.worker,workerController.getTaskByClientName) 
+router.patch('/tasks/:id',Auth.worker,workerController.updateTaskById)  //Uppdaterar ett ärende
+
+router.get('/tasks',Auth.worker,workerController.getTaskByClientName) 
 //Hämtar arbetarens ärenden.
 // Query params:
 // filter all | done
 // search Searches tasks using the clients’ name
 
-// router.get('/tasks/:id',Auth.worker,workerController.getTaskById)  //Hämtar ett ärende
-// router.patch('/tasks/:id',Auth.worker,workerController.updateTaskById)  //Uppdaterar ett ärende
-// router.get('/tasks/:id/message',Auth.worker,workerController.getTaskById)  //Hämtar alla meddelanden kopplade till ärendet,paginerat och sorterat efter tid.
-// router.post('/tasks/:id/message',Auth.worker,workerController.create) //Skapar ett nytt meddelande på ärendet
-// router.delete('/tasks/:id/message',Auth.worker,workerController.deleteUserById)  //Raderar ett meddelande
+router.get('/tasks/:id',Auth.worker,workerController.getTaskById)  //Hämtar ett ärende
+
+
 
 
 
